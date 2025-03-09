74f415103a191c5ad95bc2a1fc9614dc9134f6a2
RepulsiveAir <98040122+oski9653@users.noreply.github.com>
2025-03-09 19:30:16 +0000

    Add files via upload

---
+++ nvim/lua/custom/plugins/luarocks.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,                               -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+        opts = {
+            rocks = { "fzy", "pathlib.nvim ~> 1.0", "utils" }, -- specifies a list of rocks to install
+            -- luarocks_build_args = { "--with-lua=/my/path" }, -- extra options to pass to luarocks's configuration script
+        },
+    }
+}
---
+++ nvim/lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+  -- Lua
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      kitty = {
+        enabled = true,
+        font = "+4", -- font size increment
+      },
+    }
+  }
+}
