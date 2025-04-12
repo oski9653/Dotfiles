2ffe970d15f639804fa3f4a85093de13cdbdf912
oski9653 <oski12301@gmail.com>
2025-04-12 17:39:25 +0100

    updates

--- dot_config/nvim/lua/custom/plugins/executable_init.lua
+++ dot_config/nvim/lua/custom/plugins/executable_init.lua
@@ -1,12 +1,11 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
+	require("custom.plugins.kanagawa"),
+	require("custom.plugins.gruvbox"),
+	require("custom.plugins.fzf"),
+	require("custom.plugins.oil"),
 	require("custom.plugins.alpha-nvim"),
 	require("custom.plugins.snacks"),
 	require("custom.plugins.nvim-neorg"),
-	require("custom.plugins.nvim-tree"),
 	require("custom.plugins.zen-mode"),
 	require("custom.plugins.catppuccin"),
 	require("custom.plugins.lualine"),
